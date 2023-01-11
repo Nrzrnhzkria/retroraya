6c3de237ea3aa11444dd30f46f66eaf19f2b7b08
Nrzrnhzkria <nrz.work@gmail.com>
2023-01-11 14:38:43 +0800

    Update VendorController.php

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -177,48 +177,49 @@ class VendorController extends Controller
 
     public function create_bill($get_ic, Request $request){
         $vendor = User::where('ic_no', $get_ic)->first();
-        $payment = Membership::where('payer_id', $vendor->id)->first();
-        $booth_id = $payment->booth_id;
-        $details_id = $payment->details_id;
-        $booth_name = Booth::where('booth_id', $booth_id)->first();
-        $booth_type = BoothDetails::where('details_id', $details_id)->first();
+        // $payment = Membership::where('payer_id', $vendor->id)->first();
+        // $booth_id = $payment->booth_id;
+        // $details_id = $payment->details_id;
+        // $booth_name = Booth::where('booth_id', $booth_id)->first();
+        // $booth_type = BoothDetails::where('details_id', $details_id)->first();
 
-        $bill_id = 'ID'.uniqid();
+        // $bill_id = 'ID'.uniqid();
 
-        $payment->senangpay_id = $bill_id;
-        $payment->save();
+        // $payment->senangpay_id = $bill_id;
+        // $payment->save();
         
-        $amount = ($payment->amount)*100;
+        // $amount = ($payment->amount)*100;
 
-        $data = array(
-            'userSecretKey' => config('toyyibpay.key'),
-            'categoryCode' => config('toyyibpay.category'),
-            // 'userSecretKey' => 'a25txcs8-x59p-adcl-xwz7-1d3grr2p6c1p',
-            // 'categoryCode' => 'vokse6qd',
-            'billName' => 'HUN Vendor Registration',
-            'billDescription' => $booth_name->booth_name . ' - ' . $booth_type->booth_type,
-            'billPriceSetting' => 1,
-            'billPayorInfo' => 1,
-            'billAmount' => $amount,
-            'billReturnUrl' => 'https://hariusahawannegara.com.my/payment-status',
-            'billCallbackUrl' => 'https://hariusahawannegara.com.my/payment-callback',
-            'billExternalReferenceNo' => $bill_id,
-            'billTo' => $vendor->name,
-            'billEmail' => $vendor->email,
-            'billPhone' => $vendor->phone_no, // cannot null or 0
-            'billSplitPayment' => 0,
-            'billSplitPaymentArgs' => '',
-            'billPaymentChannel' => 2,
-            'billContentEmail' => 'Thank you for registering to HUN!',
-            'billChargeToCustomer' => 2
-        );
+        // $data = array(
+        //     'userSecretKey' => config('toyyibpay.key'),
+        //     'categoryCode' => config('toyyibpay.category'),
+        //     // 'userSecretKey' => 'a25txcs8-x59p-adcl-xwz7-1d3grr2p6c1p',
+        //     // 'categoryCode' => 'vokse6qd',
+        //     'billName' => 'HUN Vendor Registration',
+        //     'billDescription' => $booth_name->booth_name . ' - ' . $booth_type->booth_type,
+        //     'billPriceSetting' => 1,
+        //     'billPayorInfo' => 1,
+        //     'billAmount' => $amount,
+        //     'billReturnUrl' => 'https://hariusahawannegara.com.my/payment-status',
+        //     'billCallbackUrl' => 'https://hariusahawannegara.com.my/payment-callback',
+        //     'billExternalReferenceNo' => $bill_id,
+        //     'billTo' => $vendor->name,
+        //     'billEmail' => $vendor->email,
+        //     'billPhone' => $vendor->phone_no, // cannot null or 0
+        //     'billSplitPayment' => 0,
+        //     'billSplitPaymentArgs' => '',
+        //     'billPaymentChannel' => 2,
+        //     'billContentEmail' => 'Thank you for registering to HUN!',
+        //     'billChargeToCustomer' => 2
+        // );
 
-        $url = 'https://toyyibpay.com/index.php/api/createBill';
-        $response = Http::asForm()->post($url, $data);
-        $bill_code = $response->json()[0]['BillCode'];
+        // $url = 'https://toyyibpay.com/index.php/api/createBill';
+        // $response = Http::asForm()->post($url, $data);
+        // $bill_code = $response->json()[0]['BillCode'];
 
-        // dd($response->json()); // to know error
-        return redirect('https://toyyibpay.com/' . $bill_code); // return url
+        // // dd($response->json()); // to know error
+        // return redirect('https://toyyibpay.com/' . $bill_code); // return url
+        dd($vendor);
     }
 
     public function payment_status(Request $request){
